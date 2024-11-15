19c73d69759ee51281f9ccf35b834c2cea87a119
Larissa Batista <larissabatista0704@gmail.com>
2024-11-15 16:59:11 -0300

    add score, time and sound

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -7,9 +7,29 @@ const state = {
     },
     values: {
         timeId: 0,
-        gameVelocity: 800
+        gameVelocity: 800,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 10
+    },
+    actions: {
+        currentTimeDown: setInterval(currentTimeDown, 1000)
     }
 };
+
+//Funcao que controla o tempo de jogo
+function currentTimeDown(){
+
+    state.values.currentTime--;
+    state.views.time.textContent = state.values.currentTime;
+
+    if(state.values.currentTime <= 0){
+        clearInterval(state.actions.currentTimeDown);
+        clearInterval(randomSquare, state.values.gameVelocity);
+        //Mensagem Final
+        alert("FIM DE JOGO!\nSUA PONTUAÇÃO: " + state.values.result);
+    }
+}
 
 //Funcao que sortea em qual quadrado aparecera o inimigo
 function randomSquare(){
@@ -19,11 +39,13 @@ function randomSquare(){
     }); 
 
     //Sortendo um numero aleatorio
-    let radonNumber = Math.floor((Math.random() * 9));
-    let radonSquare = state.views.squares[radonNumber];
+    let randomNumber = Math.floor((Math.random() * 9));
+    let randomSquare = state.views.squares[randomNumber];
 
     //Atribuir a classe enemy para o numero sorteado
-    radonSquare.classList.add("enemy");
+    randomSquare.classList.add("enemy");
+    //Identificar o id do quadrado onde está a classe enemy
+    state.values.hitPosition = randomSquare.id;
 }
 
 //Funcao que muda o quadrado em que aparece o inimigo
@@ -31,12 +53,30 @@ function moveEnemy(){
     setInterval(randomSquare, state.values.gameVelocity);
 }
 
+//Funcao que verifica se o jogador clicou no quadrado correspondente ao inimigo, 
+//adiciona mais um a cada acerto e mostra na tela o score
 function addListenerHitBox(){
-    state.views.squares.forEach((square) => {});
-    if(square.id == (state.views.enemy.id)){}
+    state.views.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPosition){
+                state.values.result++;
+                state.views.score.textContent = state.values.result;
+                playAudio("hit.m4a");
+            }
+        });
+    });
 }
 
+//Funcao que adiciona um som
+function playAudio(audioName){
+    let audio = new Audio(`./src/audios/${audioName}`);
+    audio.volume = 0.1;
+    audio.play();
+}
+
+//Funcao que inicializa o jogo
 function init(){
     moveEnemy();
+    addListenerHitBox();
 }
 init();
